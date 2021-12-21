9a3f839f209a604b3ddf06a33becfe03bba900ce
arkhai <kiriam@bk.ru>
2021-12-21 13:17:40 +0300

    Added protos to views, routes and controllers

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,58 +1,61 @@
 class PhotosController < ApplicationController
-  before_action :set_photo, only: [:show, :edit, :update, :destroy]
+  # Для каждого действия нужно получить событие, к которому привязана фотография
+  before_action :set_event, only: [:create, :destroy]
 
-  # GET /photos
-  def index
-    @photos = Photo.all
-  end
+  # Для действия destroy нужно получить из базы саму фотографию
+  before_action :set_photo, only: [:destroy]
 
-  # GET /photos/1
-  def show
-  end
-
-  # GET /photos/new
-  def new
-    @photo = Photo.new
-  end
-
-  # GET /photos/1/edit
-  def edit
-  end
-
-  # POST /photos
+  # Действие для создания новой фотографии
+  # Обратите внимание, что фотку может сейчас добавить даже неавторизованный пользовать
+  # Смотрите домашки!
   def create
-    @photo = Photo.new(photo_params)
+    # Создаем новую фотографию у нужного события @event
+    @new_photo = @event.photos.build(photo_params)
 
-    if @photo.save
-      redirect_to @photo, notice: 'Photo was successfully created.'
+    # Проставляем у фотографии пользователя
+    @new_photo.user = current_user
+
+    if @new_photo.save
+      # Если фотографию удалось сохранить, редирект на событие с сообщением
+      redirect_to @event, notice: I18n.t('controllers.photos.created')
     else
-      render :new
+      # Если фотографию не удалось сохранить, рендер события с ошибкой
+      render 'events/show', alert: I18n.t('controllers.photos.error')
     end
   end
 
-  # PATCH/PUT /photos/1
-  def update
-    if @photo.update(photo_params)
-      redirect_to @photo, notice: 'Photo was successfully updated.'
-    else
-      render :edit
-    end
-  end
-
-  # DELETE /photos/1
+  # Действие для удаления фотографии
   def destroy
-    @photo.destroy
-    redirect_to photos_url, notice: 'Photo was successfully destroyed.'
+    message = {notice: I18n.t('controllers.photos.destroyed')}
+
+    # Проверяем, может ли пользователь удалить фотографию
+    # Если может — удаляем, нет, меняем сообщение
+    if current_user_can_edit?(@photo)
+      @photo.destroy
+    else
+      message = {alert: I18n.t('controllers.photos.error')}
+    end
+
+    # И в любом случае редиректим его на событие
+    redirect_to @event, message
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_photo
-      @photo = Photo.find(params[:id])
-    end
+  # Так как фотография — вложенный ресурс, то в params[:event_id] рельсы
+  # автоматически положает id события, которому принадлежит фотография
+  # найденное событие будет лежать в переменной контроллера @event
+  def set_event
+    @event = Event.find(params[:event_id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def photo_params
-      params.fetch(:photo, {})
-    end
+  # Получаем фотографию из базы стандартным методом find
+  def set_photo
+    @photo = @event.photos.find(params[:id])
+  end
+
+  # При создании новой фотографии мы получаем массив параметров photo
+  # c единственным полем (оно тоже называется photo)
+  def photo_params
+    params.fetch(:photo, {}).permit(:photo)
+  end
 end
