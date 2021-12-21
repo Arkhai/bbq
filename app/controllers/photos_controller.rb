ebfba6281e16a2ef5a01d411ade1c91e97393a18
arkhai <kiriam@bk.ru>
2021-12-21 21:02:51 +0300

    event-mailer, sendgrid

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -16,6 +16,7 @@ class PhotosController < ApplicationController
     @new_photo.user = current_user
 
     if @new_photo.save
+      notify_subscribers(@event, @new_photo)
       # Если фотографию удалось сохранить, редирект на событие с сообщением
       redirect_to @event, notice: I18n.t('controllers.photos.created')
     else
@@ -58,4 +59,15 @@ class PhotosController < ApplicationController
   def photo_params
     params.fetch(:photo, {}).permit(:photo)
   end
+
+  def notify_subscribers(event, photo)
+    # Собираем всех подписчиков и автора события в массив мэйлов, исключаем повторяющиеся
+    all_emails = (event.subscriptions.map(&:user_email) + [event.user.email]).uniq
+
+    # По адресам из этого массива делаем рассылку
+    # Как и в подписках, берём EventMailer и его метод comment с параметрами
+    # И отсылаем в том же потоке
+    all_emails.each do |mail|
+      EventMailer.photo(event, photo, mail).deliver_now
+    end
 end
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'bbq-everyday@goodprogrammer.ru'
   layout 'mailer'
 end
