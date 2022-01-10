13995453e83e8d7b0bdc1e19cb10c090abfc7743
arkhai <kiriam@bk.ru>
2022-01-10 16:04:37 +0300

    & instead try in emails check

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -5,20 +5,20 @@ class EventMailer < ApplicationMailer
     @name = subscription.user_name
     @event = subscription.event
 
-    mail to: subscription.event.user.email, subject: "#{t('event_mailer.subscription.title')} @#{subscription.event.title}"
+    mail to: @event.user.email, subject: "#{t('event_mailer.subscription.title')} @#{@event.title}"
   end
 
   def comment(comment, email)
     @comment = comment
     @event = comment.event
 
-    mail to: email, subject: "#{t('event_mailer.comment.title')} @#{comment.event.title}"
+    mail to: email, subject: "#{t('event_mailer.comment.title')} @#{@event.title}"
   end
 
   def photo(photo, email)
     @photo = photo
     @event = photo.event
 
-    mail to: email, subject: "#{t('event_mailer.photo.title')} @#{photo.event.title}"
+    mail to: email, subject: "#{t('event_mailer.photo.title')} @#{@event.title}"
   end
 end
