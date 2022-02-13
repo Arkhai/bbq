22bd1a1a1042ae94f81535b560e23c46d96e3a53
arkhai <kiriam@bk.ru>
2022-02-13 22:31:53 +0300

    Omniauth init

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:facebook, :vkontakte]
   has_many :events
   has_many :comments
   has_many :subscriptions
