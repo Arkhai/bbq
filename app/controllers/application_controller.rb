93ecdbe3520e99464441417011ecec4a61493ac0
arkhai <kiriam@bk.ru>
2022-03-31 19:17:23 +0300

    permit to add avatar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,11 +16,11 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(
       :sign_up,
-      keys: [:name, :email, :password, :password_confirmation]
+      keys: [:name, :email, :avatar, :password, :password_confirmation]
     )
     devise_parameter_sanitizer.permit(
       :account_update,
-      keys: [:name, :password, :password_confirmation, :current_password]
+      keys: [:name, :password, :avatar, :password_confirmation, :current_password]
     )
   end
 
